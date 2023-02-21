f2609053c235b62b60a65554a2b4601eed36811b
DinhhHau <dinhhaunguyen0812@gmail.com>
2023-02-21 10:25:37 +0700

    text antd

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import AnbIndex from './Pages/AnbIndex/AnbIndex';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AnbIndex from "./Pages/AnbIndex/AnbIndex";
+import Ex from "./Pages/Ex/Ex";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
     <Routes>
-      <Route path='' elment={<App />}>
-        <Route path='' element={<AnbIndex/>} />
-
-
+      <Route path="" elment={<App />}>
+        <Route path="" element={<AnbIndex />} />
+        <Route path="ex" element={<Ex />} />
       </Route>
     </Routes>
   </BrowserRouter>
